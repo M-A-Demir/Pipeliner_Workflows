4fe60d26d0ed1d2265673234b49ed3df597c6493
M-A-Demir <119374201+M-A-Demir@users.noreply.github.com>
2026-04-20 12:38:54 +0100

    Create sbatch_compress.sh
    Slurm script for running benchmarking jobs on Diamond cluster (Wilson)

---
+++ sbatch_compress.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH -p cs05r
+#SBATCH -J 'compress'
+#SBATCH -o output.log
+#SBATCH -e error.log
+#SBATCH -t 72:00:00
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=40
+
+DIRS_TO_COMPRESS=("/dls/m12/data/2026/cm44187-1/spool/betagal/MotionCorr/job002/Movies/GridSquare_27367468/Data"
+"/dls/m12/data/2026/cm44187-1/spool/betagal/MotionCorr/job002/Movies/GridSquare_27367466/Data"
+"/dls/m12/data/2026/cm44187-1/spool/betagal/MotionCorr/job002/Movies/GridSquare_27367464/Data"
+"/dls/m12/data/2026/cm44187-1/spool/betagal/MotionCorr/job002/Movies/GridSquare_27367177/Data")
+
+
+OUTDIR=/dls/m12/data/2025/cm40657-1/out_compressed
+COMPRESSION_SCHEMES=("terse" "pbzip2" "blosc-zstd" "blosc2-zstd")
+
+for d in "${DIRS_TO_COMPRESS[@]}"; do
+
+  for scheme in "${COMPRESSION_SCHEMES[@]}"; do
+    echo "Processing $d"
+
+    rm -f ${OUTDIR}/*.tif*
+    rm -f ${OUTDIR}/*.mrc*
+
+    if ! srun python -m ccp_dcompress.scripts.run_compression --compression_scheme ${scheme} --unit "GB" --filepath ${d} --outdir ${OUTDIR}; then
+      echo "FAILED: $d with $scheme — skipping"
+      continue
+    fi
+
+
+    #srun python -m ccp_dcompress.scripts.run_compression --compression_scheme ${COMPRESSION_SCHEME} --unit "GB" --filepath ${DIR_TO_COMPRESS} --outdir ${OUTDIR}
+    #srun emencode_compress -s ${scheme} -i ${d} -o ${OUTDIR} -u GB
+
+    done
+done
+
+echo "Done."
